b56d79a166dc08833accfdb6f480e5f97385e43f
liquidgoo <60588318+liquidgoo@users.noreply.github.com>
2021-12-22 00:44:47 +0300

    Add files via upload

---
+++ TestClassGeneratorProject/FileWithContent.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestClassGeneratorProject
+{
+    public class FileWithContent
+    {
+        public string Path { get; }
+
+        public string Content { get; }
+
+        public FileWithContent(string path, string content)
+        {
+            Path = path;
+            Content = content;
+        }
+    }
+}
