b53c6d1638ccca5fa45e0e58265b474785b89802
liquidgoo <60588318+liquidgoo@users.noreply.github.com>
2021-12-22 00:21:46 +0300

    Add files via upload

---
+++ FileInputOutputProject/MainClass.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using System;
+
+namespace FileInputOutputProject.TestFiles
+{
+    class MainClass
+    {
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            int maxParallelism = 5;
+            string[] files = Directory.GetFiles("D:\\smth\\походу уник\\spp\\spp4\\ExperimentProject", "*.cs");
+            var configuration = new PipelineConfiguration(maxReadingTasks: maxParallelism,
+                maxProcessingTasks: maxParallelism,
+                maxWritingTasks: maxParallelism);
+
+            var pipeline = new Pipeline(configuration);
+            await pipeline.PerformProcessing(files);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ FileInputOutputProject/bin/Debug/TestFilesOutput/ProgramTest.cs
@@ -0,0 +1,12 @@
+using System.Generics;
+using NUnit.Framework;
+using Moq;
+
+namespace GeneratedTestClasses
+{
+    [TestFixture]
+    class ProgramTest
+    {
+        private Program _program;
+    }
+}
