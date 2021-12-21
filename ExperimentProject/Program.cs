3f62340ccee50c73508e2ddd2264aa6da78b3312
liquidgoo <60588318+liquidgoo@users.noreply.github.com>
2021-12-22 00:22:11 +0300

    Add files via upload

---
+++ ExperimentProject/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExperimentProject
+{
+    public interface IData
+    {
+        int calc();
+    }
+
+    class Implementation : IData
+    {
+        public int calc()
+        {
+            return 2 * 2;
+        }
+    }
+
+    public class TestedClass
+    {
+        public int foo(int d)
+        {
+            return data.calc() * d;
+        }
+
+        public TestedClass(IData data)
+        {
+            this.data = data;
+        }
+            
+        private IData data;
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
